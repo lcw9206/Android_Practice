4aa802029ecedd7ba02fa9e01f396a43a815eeec
Cheolwoo Lee <lcw9206@gmail.com>
2020-04-17 17:38:47 +0900

    Practice First Commit

---
+++ app/src/main/java/com/example/practice/appstore/AppStoreActivity.java
@@ -0,0 +1,16 @@
+package com.example.practice.appstore;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.practice.R;
+
+public class AppStoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appstore);
+    }
+}
---
+++ app/src/main/java/com/example/practice/appstore/AppStoreAdapter.java
@@ -0,0 +1,4 @@
+package com.example.practice.appstore;
+
+public class AppStoreAdapter {
+}
